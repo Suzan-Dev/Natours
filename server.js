2c1ff678d5bc45bccae0935c2eb6af088f88e5cf
Sujan Tamang <everestgaming3@gmail.com>
2021-02-03 07:37:54 +0545

    SIGTERM listener added

--- server.js
+++ server.js
@@ -37,3 +37,10 @@ process.on('unhandledRejection', (err) => {
     process.exit(1);
   });
 });
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM RECEIVED! Shutting down the server...');
+  server.close(() => {
+    console.log('Process terminated!');
+  });
+});
